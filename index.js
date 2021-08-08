655991f65c9e3c69287012435584e9d26e0bcb6e
alexisflach <alexis.flach87@gmail.com>
2021-08-08 20:01:57 +0200

    small fix

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 const config = require('./config');
 
 const express = require('express');
-
-
 const app = express();
 
 app.get('/', (_, res) => {
   res.send("Hello World")
 })
-
-
 
 app.listen(config.port, () => {
   console.log(`Server is listening on port: ${config.port}`);
